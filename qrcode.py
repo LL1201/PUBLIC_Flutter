2cd0fa5d73e959ac1bfabd9fca706a23e938d530
unknown <in5b11@ILIDOM2018.LOCAL>
2022-03-10 15:20:50 +0100

    .

--- qrcode.py
+++ qrcode.py
@@ -13,13 +13,12 @@ if(not ret):
 
 while(cap.isOpened()):
     _, frame = cap.read()
-    if(rec):
-        data, points, _ = decoder.detectAndDecode(frame)
-        if points is not None:
-            print('Decoded data: ' + data)
- 
-            points = points[0]
-            for i in range(len(points)):
-                pt1 = [int(val) for val in points[i]]
-                pt2 = [int(val) for val in points[(i + 1) % 4]]
-                cv2.line(frame, pt1, pt2, color=(255, 0, 0), thickness=3)
+    cv2.imshow("webcam", frame)
+    data, points, _ = decoder.detectAndDecode(frame)
+    if points is not None:
+        print('Decoded data: ' + data) 
+        points = points[0]
+        for i in range(len(points)):
+            pt1 = [int(val) for val in points[i]]
+            pt2 = [int(val) for val in points[(i + 1) % 4]]
+            cv2.line(frame, pt1, pt2, color=(255, 0, 0), thickness=3)
