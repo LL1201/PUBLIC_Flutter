b7730da476a3e55ac74aba41a80b0c1237ffe363
unknown <in5b11@ILIDOM2018.LOCAL>
2022-03-24 09:02:00 +0100

    .

--- src/demoserver/DemoServer.java
+++ src/demoserver/DemoServer.java
@@ -27,7 +27,7 @@ public class DemoServer extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("<html><title>Supersballo</title><head><h1>Supersballo Web App</h1></head><hr><body>Powered by me :)</body></html>");
 	}
 
 	/**
