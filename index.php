9c1428dc162cc25a4c4fbe2f1bec4a48f5b5404a
LL1201 <luca.loner04@gmail.com>
2022-03-27 17:46:05 +0200

    fine

--- index.php
+++ index.php
@@ -38,26 +38,14 @@
  <body>
      <article>
          <p>
-             Un'associazione “Banca del Tempo” vuole realizzare una base di dati per registrare e gestire le attività dell'associazione.
-             La “Banca del Tempo” (BdT) indica uno di quei sistemi organizzati di persone che si associano per scambiare servizi e/o saperi, attuando un aiuto reciproco.
+             “Banca del Tempo” è un'associazione basata sullo scambio di servizi/prestazioni, attuando un aiuto reciproco.
              Attraverso la BdT le persone mettono a disposizione il proprio tempo per determinate prestazioni (effettuare una piccola riparazione in casa, preparare una torta, conversare in lingua straniera, ecc.) aspettando di ricevere prestazioni da altri.
              Non circola denaro, tutte le prestazioni sono valutate in tempo, anche le attività di segreteria.
              Le prestazioni sono suddivise in categorie (lavori manuali, tecnologie, servizi di trasporto, bambini, attività sportive, ecc.).
-             Chi dà un' ora del suo tempo a qualunque socio, riceve un' ora di tempo da chiunque faccia parte della BdT. La base di dati dovrà mantenere le informazioni relative ad ogni prestazione (quale prestazione, da chi è stata erogata, quale socio ha ricevuto quella prestazione, per quante ore e in quale data) per consentire anche interrogazioni di tipo statistico.
-             Il territorio di riferimento della BdT è limitato (un quartiere in una grande città o un piccolo comune) ed è suddiviso in zone; la base di dati dovrà contenere la mappa del territorio e delle singole zone, in forma grafica.
-             <br>
-             Si consideri la realtà di riferimento sopra descritta e si realizzino:
-             <br>
-             <li> la progettazione concettuale della realtà indicata attraverso la produzione di uno schema (ad esempio ER, Entity - Relationship) con gli attributi di ogni entità, il tipo di ogni relazione e i suoi eventuali attributi;</li>
-             <li> una traduzione dello schema concettuale realizzato in uno schema logico (ad esempio secondo uno schema relazionale);</li>
-             <li> le seguenti interrogazioni espresse in algebra relazionale e/o in linguaggio SQL:</li>
-             <li> Dato un servizio estrarre tutte le info delle prestazioni relative a quel servizio;</li>
-             <li> Data una zona estrarre tutti i servizi offerti in quella zona;</li>
-             <li> data una richiesta di prestazione, visualizzare la porzione di mappa del territorio nel quale si trova il socio richiedente e l’elenco di tutti i soci che si trovano in quella zona in grado di erogare quella prestazione, visualizzandone il nome, cognome, indirizzo e numero di telefono;</li>
-             <li> visualizzare tutti i soci che fanno parte della segreteria e che offrono anche altri tipi di prestazione;</li>
-             <li> produrre un elenco delle prestazioni ordinato in modo decrescente secondo il numero di ore erogate per ciascuna prestazione.</li>
-             <li> produrre l’elenco dei soci (con cognome, nome e telefono) che hanno un “debito” nella BdT (coloro che hanno usufruito di ore di prestazioni in numero superiore a quelle erogate);</li>
+             Chi dà un' ora del suo tempo a qualunque socio, riceve un' ora di tempo da chiunque faccia parte della BdT.
+             Il territorio di riferimento della BdT è limitato (un quartiere in una grande città o un piccolo comune) ed è suddiviso in zone.
          </p>
+         <img src="imageHome.jpg">
      </article>
 
  </body>
